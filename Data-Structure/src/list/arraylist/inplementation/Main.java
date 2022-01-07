248f4f68fce1bd3a73cd4ecd1d6708df89e1cf2d
c04nh <w2038@e-mirim.hs.kr>
2022-01-07 19:35:11 +0900

    파트4. ArrayList 직접 구현하기 / 파트5. 과제

--- Data-Structure/src/list/arraylist/inplementation/Main.java
+++ Data-Structure/src/list/arraylist/inplementation/Main.java
@@ -10,6 +10,14 @@ public class Main {
 		numbers.addLast(40);
 		numbers.add(1, 15);
 		numbers.addFirst(5);
+		System.out.println(numbers.get(0));
+		System.out.println(numbers.get(1));
+		System.out.println(numbers.get(2));
+		System.out.println(numbers.get(3));
+		System.out.println(numbers);
+		System.out.println(numbers.size());
+		System.out.println(numbers.indexOf(20));
+		System.out.println(numbers.indexOf(70));
 	}
 
 }
